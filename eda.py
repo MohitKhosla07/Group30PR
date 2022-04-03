99082eff58b335155b587aeb2686a01d950fb95c
Himanshu8596 <himanshukr8596@gmail.com>
2022-04-03 13:27:09 +0530

    Visualization Part

--- eda.py
+++ eda.py
@@ -31,3 +31,55 @@ rdd_table = df_features.rdd.map(lambda row: row[0:])
 corr_mat=Statistics.corr(rdd_table, method="pearson")
 print(corr_mat)
 fra20.corr(col1='highway_fuel_economy', col2='city_fuel_economy', method='pearson')
+
+# Visualization
+
+fig=plt.subplots(figsize=(100, 100))
+
+for i, feature in enumerate(['fuel_tank_volume', 'height', 'length', 'mileage', 'owner_count','torque', 'width', 'maximum_seating', 'fueltype_id ','bodytype_id ', 'enginetype_id', 'wheelsystem_id', 'price']):
+    plt.subplot(20, 20, i+1)
+    plt.subplots_adjust(hspace = 2.0)
+    sns.distplot(pd_data[feature])
+    plt.tight_layout()
+
+
+def histogram(fra20, col, bins=10, xname=None, yname=None):
+    
+    '''
+    This function makes a histogram from spark dataframe named 
+    df for column name col. 
+    '''
+    
+    # Calculating histogram in Spark 
+    vals = fra20.select(col).rdd.flatMap(lambda x: x).histogram(bins)
+    
+    # Preprocessing histogram points and locations 
+    width = vals[0][1] - vals[0][0]
+    loc = [vals[0][0] + (i+1) * width for i in range(len(vals[1]))]
+     
+    # Making a bar plot 
+    plt.bar(loc, vals[1], width=width)
+    plt.xlabel(col)
+    plt.ylabel(yname)
+    plt.show()
+
+
+histogram(fra20, 'mileage', bins=15, yname='enginetype_id')
+
+
+histogram(fra20, 'city_fuel_economy', bins=15, yname='mileage')
+
+
+histogram(fra20, 'width', bins=15, yname='height')
+
+
+histogram(fra20, 'width', bins=15, yname='torque')
+
+
+histogram(fra20, 'engine_displacement', bins=15, yname='enginetype_id')
+
+
+histogram(fra20, 'enginetype_id', bins=15, yname='bodytype_id ')
+
+
+histogram(fra20, 'bodytype_id ', bins=15, yname='price')
